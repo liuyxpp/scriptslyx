7ed20ffb818f702e3bd9ee846bf9a31ab3c8c114
Yi-Xin Liu <liuyxpp@gmail.com>
2011-01-04 17:56:19 +0800

    fix a bug while read nodes from file

---
+++ submit.php
@@ -0,0 +1,7 @@
+<?php
+    $exec_name="test";
+    $exec_path="/export/home/lyx/project/sandbox/test";
+    $exec_log="log_test";
+    $node="c0109";
+    `./launch $exec_name $exec_path $exec_log $node> log_launch &`
+?>
